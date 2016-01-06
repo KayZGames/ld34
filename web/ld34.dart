01acaf689501847485812ecb4efde8584a8b272b
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2016-01-06 13:37:16 +0100

    hide button while game is loading

--- web/ld34.dart
+++ web/ld34.dart
@@ -1,5 +1,7 @@
 import 'package:ld34/client.dart';
 void main() {
+  querySelector('#loading').style.display = 'none';
+  (querySelector('#startGame') as ButtonElement).style.display = 'inline-block';
   querySelector('#startGame').onClick.listen((_) {
     querySelector('#story').style.opacity = '0.0';
     querySelector('#game').style.opacity = '1.0';
